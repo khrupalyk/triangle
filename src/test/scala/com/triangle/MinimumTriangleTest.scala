f254eb15b6244f34ac615cc39f1727dc6fe0fc3a
akhrupalyk <akhrupalyk@demandbase.com>
2023-01-29 11:09:44 +0200

    Implemented function to get minimal path sum for the tree

--- src/test/scala/com/triangle/MinimumTriangleTest.scala
+++ src/test/scala/com/triangle/MinimumTriangleTest.scala
@@ -3,8 +3,42 @@ package com.triangle
 import org.scalatest.funsuite.AnyFunSuite
 
 class MinimumTriangleTest extends AnyFunSuite {
+
+  def minTriangle(data: Array[Array[Int]]): Int = {
+
+    def process(tree: Array[Array[Int]], i: Int, j: Int): Int = {
+
+      if(tree.length == i) return 0
+
+      val node = tree(i)(j) + Math.min(process(tree, i + 1, j), process(tree, i + 1, j + 1))
+
+      node
+
+    }
+
+    process(data, 0, 0)
+  }
+
+
   test("first test") {
-    assert(true)
+    val data = Array(
+         Array(7),
+        Array(6,3),
+       Array(3,8,5),
+      Array(11,2,10,9)
+    )
+
+    assert(minTriangle(data) == 18)
+
+
+    val data2 = Array(
+         Array(7),
+        Array(6, 3),
+       Array(3, 8, 5),
+      Array(11, 12, 10, 9)
+    )
+
+    assert(minTriangle(data2) == 24)
   }
 
 }
