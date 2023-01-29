db00cac434f0622789b7579ab3a3a9b2c25c3368
akhrupalyk <akhrupalyk@demandbase.com>
2023-01-29 13:54:16 +0200

    Using loop instead of recursion for better performance

--- src/test/scala/com/triangle/MinimumTriangleTest.scala
+++ src/test/scala/com/triangle/MinimumTriangleTest.scala
@@ -8,59 +8,74 @@ class MinimumTriangleTest extends AnyFunSuite {
     io.Source.fromResource(fileName).getLines().map(_.split(" ").map(_.toInt)).toArray
   }
 
-  type Index = Int
+  def minTriangle(data: Array[Array[Int]]): List[Int] = {
 
-  def minTriangle(data: Array[Array[Int]]): List[Index] = {
+    if(data.length <= 1)
+      return data.headOption.map(_.toList).getOrElse(Nil)
 
-    type Sum = Int
+    val sums = new Array[Int](data.length)
+    val values = new Array[Int](data.length)
 
-    def process(tree: Array[Array[Int]], i: Int, j: Int): (Sum, List[Index]) = {
-
-      if(tree.length == i) return (0, Nil)
-
-      val x1 = process(tree, i + 1, j)
-      val x2 = process(tree, i + 1, j + 1)
-
-      val totalSum: Sum = tree(i)(j) + Math.min(x1._1, x2._1)
-      val indexes: List[Index] = j :: (if(x1._1 <= x2._1) x1._2 else x2._2)
-
-      (totalSum, indexes)
+    for(i <- sums.indices) {
+      sums(i) = data(sums.length - 1)(i)
     }
 
-    val result = process(data, 0, 0)
+    var i = data.length - 2
 
-    result._2.zipWithIndex map { case (j, i) =>
-      data(i)(j)
+    while (i >= 0) {
+
+      var j = 0
+      var prevMinNodeSum = data(i)(j) + sums(j) + sums(j + 1)
+      var foundMinValue = 0
+      while(j < data(i).length) {
+        val nodeSum = data(i)(j) + sums(j) + sums(j + 1)
+        if (prevMinNodeSum >= nodeSum) {
+          if (sums(j) >= sums(j + 1)) {
+            foundMinValue = data(i + 1)(j + 1)
+          } else {
+            foundMinValue = data(i + 1)(j)
+          }
+          prevMinNodeSum = nodeSum
+        }
+
+        sums(j) = data(i)(j) + Math.min(sums(j), sums(j+1))
+        j = j + 1
+      }
+      values(i+1) = foundMinValue
+      i = i - 1
     }
+
+    values(0) = data.head.head
+    values.toList
   }
 
 
-  test("first test") {
-    val data = Array(
-         Array(7),
-        Array(6,3),
-       Array(3,8,5),
-      Array(11,2,10,9)
+  test("small triangles") {
+
+    val data1 = Array(
+      Array(7),
+      Array(6, 3),
+      Array(3, 8, 5),
+      Array(11, 2, 10, 9)
     )
 
-    println(minTriangle(data))
-    assert(minTriangle(data) == List(7, 6, 3, 2))
-
+    assert(minTriangle(data1) == List(7, 6, 3, 2))
 
     val data2 = Array(
-         Array(7),
-        Array(6, 3),
-       Array(3, 8, 5),
+      Array(7),
+      Array(6, 3),
+      Array(3, 8, 5),
       Array(11, 12, 10, 9)
     )
 
-    assert(minTriangle(data2) == List(7,3,5,9))
+    assert(minTriangle(data2) == List(7, 3, 5, 9))
+
+    val data3 = loadData("tests/data_small.txt")
+    assert(minTriangle(data3) == (1 to data3.length).map(_ => 1).toList)
   }
 
-  test("Awd") {
-    val data = loadData("tests/data_small.txt")
-    println(data.length)
-    println(minTriangle(data))
+  test("big triangle") {
+    val data = loadData("tests/data_big.txt")
+    assert(minTriangle(data) == (1 to data.length).map(_ => 1).toList)
   }
-
 }
